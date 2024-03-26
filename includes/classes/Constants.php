a4f33c7adaab64941758a6361dcb7e75c6fda552
regivise <reginald.odhiambo@gmail.com>
2024-03-26 08:46:17 +0300

    Styled Error Message

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -4,11 +4,11 @@ class Constants{
     public static $lastNameCharacters ="Last name should be between 5 and 25 characters!";
     public static $userNameCharacters ="Username should be between 5 and 25 characters!";
     public static $userNameTaken ="Username already exists!";
-    public static $emailsDontMatch = "Your emails don't match";
+    public static $emailsDontMatch = "Emails don't match";
     public static $emailInvalid = "Invalid email";
     public static $emailTaken = "Email already in use";
     public static $passwordsDontMatch = "Passwords don't match";
-    public static $passwordLength = "Your password must be between 5 and 25 characters";
+    public static $passwordLength = "Password must be between 5 and 25 characters";
 }
 
 
